3e25ee71b834649d0c69b7fadd7413ee94863236
davidutbaiatfinut <146932119+davidutbaiatfinut@users.noreply.github.com>
2023-11-30 14:14:32 +0200

    minor changes

---
+++ src/cmake-build-debug/CMakeFiles/MyBoostProject.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/davidonofrei/Desktop/dev/C-WebSocket/src/DataStream.cpp" "CMakeFiles/MyBoostProject.dir/DataStream.cpp.o" "gcc" "CMakeFiles/MyBoostProject.dir/DataStream.cpp.o.d"
+  "/Users/davidonofrei/Desktop/dev/C-WebSocket/src/HTTPServer.cpp" "CMakeFiles/MyBoostProject.dir/HTTPServer.cpp.o" "gcc" "CMakeFiles/MyBoostProject.dir/HTTPServer.cpp.o.d"
+  "/Users/davidonofrei/Desktop/dev/C-WebSocket/src/WebSocketServer.cpp" "CMakeFiles/MyBoostProject.dir/WebSocketServer.cpp.o" "gcc" "CMakeFiles/MyBoostProject.dir/WebSocketServer.cpp.o.d"
+  "/Users/davidonofrei/Desktop/dev/C-WebSocket/src/main.cpp" "CMakeFiles/MyBoostProject.dir/main.cpp.o" "gcc" "CMakeFiles/MyBoostProject.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-release/CMakeFiles/MyBoostProject.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyBoostProject.dir/DataStream.cpp.o"
+  "CMakeFiles/MyBoostProject.dir/DataStream.cpp.o.d"
+  "CMakeFiles/MyBoostProject.dir/HTTPServer.cpp.o"
+  "CMakeFiles/MyBoostProject.dir/HTTPServer.cpp.o.d"
+  "CMakeFiles/MyBoostProject.dir/WebSocketServer.cpp.o"
+  "CMakeFiles/MyBoostProject.dir/WebSocketServer.cpp.o.d"
+  "CMakeFiles/MyBoostProject.dir/main.cpp.o"
+  "CMakeFiles/MyBoostProject.dir/main.cpp.o.d"
+  "MyBoostProject"
+  "MyBoostProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyBoostProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
